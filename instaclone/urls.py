ecdd4df6a313694acfc9c02c15d0194e456d6ccc
Vivek Kumar <viveKumar391@gmail.com>
2017-08-02 12:05:54 +0530

    Updated

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from myapp.views import signup_view,login_view, feed_view, post_view
+from myapp.views import signup_view,login_view, feed_view, post_view,like_view, comment_view, logout_view,upvote_view
+#define urls
 urlpatterns = [
+    url('logout/',logout_view),
+    url('upvote/', upvote_view),
     url('post/', post_view),
     url('feed/', feed_view),
+    url('like/', like_view),
+    url('comment/', comment_view),
     url('login/', login_view),
     url('', signup_view)
 ]
